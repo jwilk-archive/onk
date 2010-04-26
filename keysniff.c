a0fda1d9272e315d1d5d8af907ed7bfaee91a5d8
Jakub Wilk <jwilk@jwilk.net>
2010-04-26 23:11:59 +0200

    Update my e-mail address.

--- keysniff.c
+++ keysniff.c
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2005, 2006, 2007, 2008 Jakub Wilk <ubanus@users.sf.net>.
+ * Copyright © 2005, 2006, 2007, 2008 Jakub Wilk <jwilk@jwilk.net>.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
