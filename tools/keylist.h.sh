d068a0ad4c1710ba139d513a6cd27a25cccece50
Jakub Wilk <jwilk@jwilk.net>
2017-06-17 18:26:46 +0200

    tools: use sed -E instead of -r.
    -E is more portable.
    
    http://austingroupbugs.net/view.php?id=528

--- tools/keylist.h.sh
+++ tools/keylist.h.sh
@@ -26,7 +26,7 @@ typedef struct { char *name; int value; } keylist_item_t;
 static const keylist_item_t keylist[] =
 {
 EOF
-sed -n -r -e '/^#define (KEY|BTN)_/ s/#define ([A-Z0-9_]+)[ \t]*((0x)?[0-9a-f]+)$/ { .name = "\1", .value = \2 },/p' | sort
+sed -n -E -e '/^#define (KEY|BTN)_/ s/#define ([A-Z0-9_]+)[ \t]*((0x)?[0-9a-f]+)$/ { .name = "\1", .value = \2 },/p' | sort
 cat <<EOF
 };
 
